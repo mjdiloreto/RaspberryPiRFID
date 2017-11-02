dafc4c20dadc4c9c31a2b9fb93791d43b504fbf7
Lilyn <jy4gao@gmail.com>
2017-11-02 18:49:25 -0400

    Added "addMember" method for command line UI.

--- command_line.py
+++ command_line.py
@@ -1,5 +1,5 @@
 import cmd
-
+from save_funcs import *
 
 class RFID_Cmd(cmd.Cmd):
 
@@ -7,6 +7,16 @@ class RFID_Cmd(cmd.Cmd):
         """This command says hello."""
         print("hello " + line)
 
+    def do_addMember(self, line):
+        """This command allows new members to add their card ID and name to the NU Wireless database."""
+        print("Tap the NU ID card:" + line)
+        # nuID = getUID()
+        nuID = 0
+        firstName = input('Enter first name:')
+        lastName = input('Enter last name:')
+        ex = input('Enter directory to csv file:')
+        # ex = "example.csv"
+        save_funcs.save(ex,lastName,firstName,nuID)
 
 if __name__ == '__main__':
     c = RFID_Cmd()
